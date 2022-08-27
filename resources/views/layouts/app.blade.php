841f165d3c05bb76c59059443144106c4f413d9a
ar2r-java <roooob66666666666@gmail.com>
2022-08-27 14:11:55 +0300

    bootstrap row

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
 </head>
 <body>
     @include('inc.header')
-    <div class="container">
+    <div class="container mt-5">
         <div class="row">
             <div class="col-8">
                 @yield('content')
