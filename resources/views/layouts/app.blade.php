68e1b07fca50a95e0d74865546ba3266b97954eb
ar2r-java <roooob66666666666@gmail.com>
2022-08-27 13:51:44 +0300

    css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('/css/app.css')}}">
     <title>@yield('title-block')</title>
 </head>
 <body>
     @yield('content')
+
+    @include('inc.aside')
+
 </body>
 </html>
